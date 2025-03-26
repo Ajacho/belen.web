9356185a8a2345f8f41bf411c0dad243416408cc
ajacho <belen.jacho18@gmail.com>
2025-03-26 09:40:52 -0700

    pushing changes to branch dev in github

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div class name='topnav'>
+            {/* <div href='#home'>Andrea B. Jacho</div> */}
+            <a href='#home'>Home</a>
+            <a href='#about'>About</a>
+            <a href='#experience'>Experience</a>
+            <a href='#projects'>Projects</a>
+
+        </div>
+    );
+}
+ 
+export default NavBar;
